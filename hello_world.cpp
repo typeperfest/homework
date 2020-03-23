253b69cf12115fe32c338df7e178ee40e9725563
typeperfest <perfect2111@gmail.com>
2020-03-23 12:59:56 +0300

    Adds comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	std::string name;
-	std::cin >> name;
-	std::cout << "Hello world from " << name << std::endl;
+	std::string name; // Defenition if name
+	std::cin >> name; // Entering the name 
+	std::cout << "Hello world from " << name << std::endl; // Main task
 	return 0;
 }
