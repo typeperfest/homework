2f00c2932fdb08773084032806d6747a0c0f9eb6
typeperfest <perfect2111@gmail.com>
2020-03-23 13:32:18 +0300

    fot conflicts

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	std::string name; // Defenition if name
-	std::cin>>name; // Entering the name 
-	std::cout<<"Hello world from "<<name<<std::endl; // Main task
+	std::string name; // Defenition, if name
+	std::cin>>name; // Entering the имя 
+	std::cout<<"Hello world from "<<name<<std::endl; // Main, task
 	return 0;
 }
