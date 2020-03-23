5ac81add6cdef7414c187126f99eec98c76e42cc
typeperfest <perfect2111@gmail.com>
2020-03-23 13:01:24 +0300

    Brokes the lines

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	std::string name; // Defenition if name
-	std::cin >> name; // Entering the name 
-	std::cout << "Hello world from " << name << std::endl; // Main task
+	std::cin>>name; // Entering the name 
+	std::cout<<"Hello world from "<<name<<std::endl; // Main task
 	return 0;
 }
