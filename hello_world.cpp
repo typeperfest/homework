d1e9357c45bd4fb15e5b7887fa57fee74ee1e093
typeperfest <perfect2111@gmail.com>
2020-03-23 12:57:10 +0300

    Adds defenitions

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	std::string name;
-	std::cin >> name;
-	std::cout << "Hello world from " << name << std::endl;
+	std::string name; // Defenition of string
+	std::cin >> name; // Entering the name
+	std::cout << "Hello world from " << name << std::endl; // Main Task
 	return 0;
 }
